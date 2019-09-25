7044e0a3eb6256d91a9a128a68d06cf602f2c2e7
mjalenqu <martin.jalenques@wanadoo.fr>
2019-09-25 10:52:09 +0200

    norme on exec/var.c

--- src/exec/var.c
+++ src/exec/var.c
@@ -6,7 +6,7 @@
 /*   By: mjalenqu <mjalenqu@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/09/16 14:49:17 by mjalenqu     #+#   ##    ##    #+#       */
-/*   Updated: 2019/09/25 09:56:02 by mjalenqu    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/09/25 10:30:11 by mjalenqu    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -71,17 +71,31 @@ void		add_env(t_var **var, char *str)
 	}
 	if (!(*var))
 	{
-		(*var) = malloc(sizeof(t_var));
-		prev->next = (*var);
-		(*var)->next = NULL;
-		(*var)->name = name;
-		(*var)->data = init_data(str);
-		(*var)->type = LOCAL;
+		add_local(var, str, prev);
 		return ;
 	}
 	ft_strdel(&(*var)->data);
 	ft_strdel(&name);
 	(*var)->data = init_data(str);
+}
+
+int			local_or_env(t_var **var, char **cmd, int i, char ***tmp)
+{
+	if (cmd[i] && find_equal(cmd[i]))
+	{
+		if (check_cmd(cmd) == 1)
+		{
+			add_env_temp(var, cmd[i], TEMP);
+			*tmp = remove_tab(cmd, i);
+		}
+		else
+		{
+			add_env(var, cmd[i]);
+			ft_free_tab(cmd);
+			return (-1);
+		}
+	}
+	return (0);
 }
 
 char		**check_exec_var(char **cmd, t_var **var)
@@ -93,20 +107,8 @@ char		**check_exec_var(char **cmd, t_var **var)
 	tmp = NULL;
 	while (cmd[i])
 	{
-		if (cmd[i] && find_equal(cmd[i]))
-		{
-			if (check_cmd(cmd) == 1)
-			{
-				add_env_temp(var, cmd[i], TEMP);
-				tmp = remove_tab(cmd, i);
-			}
-			else
-			{
-				add_env(var, cmd[i]);
-				ft_free_tab(cmd);
-				return (NULL);
-			}
-		}
+		if (local_or_env(var, cmd, i, &tmp) == -1)
+			return (NULL);
 		i++;
 	}
 	if (tmp != NULL)
