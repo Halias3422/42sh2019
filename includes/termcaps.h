d0bc5aa607d9b886dc4c747f0b83556f66883541
rlegendr <legendre.rodolphe12@gmail.com>
2019-11-12 13:30:47 +0100

    historic expansion sublimed

--- includes/termcaps.h
+++ includes/termcaps.h
@@ -6,7 +6,7 @@
 /*   By: mjalenqu <mjalenqu@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/03/28 09:15:13 by mjalenqu     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/12 12:52:09 by rlegendr    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/12 13:24:19 by rlegendr    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -487,7 +487,7 @@ void					jump_up(t_pos *pos);
 /*
 ** HISTORY_EXPANSION.C
 */
-void					check_history_expansion(t_pos *pos, t_hist *hist, int i,
+int						check_history_expansion(t_pos *pos, t_hist *hist, int i,
 						int error);
 int						replace_expansion_by_value(t_pos *pos, t_hist *hist,
 						int i, int error);
